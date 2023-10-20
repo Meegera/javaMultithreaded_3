9ec9a0a0c553a917d457c9d66fa14ff8e06131b9
Greta Donskova <gretadonskova@gmail.com>
2023-10-21 02:23:46 +0300

    change Main

--- src/main/java/org/task1/Main.java
+++ src/main/java/org/task1/Main.java
@@ -2,12 +2,12 @@ package org.task1;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 public class Main {
     public static AtomicInteger counterLength3 = new AtomicInteger(0);
     public static AtomicInteger counterLength4 = new AtomicInteger(0);
     public static AtomicInteger counterLength5 = new AtomicInteger(0);
+
     public static void main(String[] args) {
         Random random = new Random();
         String[] texts = new String[100_000];
@@ -16,30 +16,42 @@ public class Main {
         }
         //Stream.of(texts).forEach(System.out::println);
 
-        Thread threadLength3 = new Thread(() -> {
-            for(int i=0; i < texts.length; i++){
-                if(texts[i].length() == 3){
-                    if(isPalindrome(texts[i]) || isOneLetter(texts[i]) || isAscendingRow(texts[i])){
+        Thread threadCriteria1 = new Thread(() -> {
+            for (int i = 0; i < texts.length; i++) {
+                if (isPalindrome(texts[i])) {
+                    if(texts[i].length() == 3) {
                         counterLength3.getAndIncrement();
-                    }
-                }
-            }
-        });
-
-        Thread threadLength4 = new Thread(() -> {
-            for(int i=0; i < texts.length; i++){
-                if(texts[i].length() == 4){
-                    if(isPalindrome(texts[i]) || isOneLetter(texts[i]) || isAscendingRow(texts[i])){
+                    } else if (texts[i].length() == 4) {
                         counterLength4.getAndIncrement();
+                    } else {
+                        counterLength5.getAndIncrement();
                     }
                 }
             }
         });
 
-        Thread threadLength5 = new Thread(() -> {
-            for(int i=0; i < texts.length; i++){
-                if(texts[i].length() == 5){
-                    if(isPalindrome(texts[i]) || isOneLetter(texts[i]) || isAscendingRow(texts[i])){
+        Thread threadCriteria2 = new Thread(() -> {
+            for (int i = 0; i < texts.length; i++) {
+                if (isOneLetter(texts[i])) {
+                    if(texts[i].length() == 3) {
+                        counterLength3.getAndIncrement();
+                    } else if (texts[i].length() == 4) {
+                        counterLength4.getAndIncrement();
+                    } else {
+                        counterLength5.getAndIncrement();
+                    }
+                }
+            }
+        });
+
+        Thread threadCriteria3 = new Thread(() -> {
+            for (int i = 0; i < texts.length; i++) {
+                if (isAscendingRow(texts[i])) {
+                    if(texts[i].length() == 3) {
+                        counterLength3.getAndIncrement();
+                    } else if (texts[i].length() == 4) {
+                        counterLength4.getAndIncrement();
+                    } else {
                         counterLength5.getAndIncrement();
                     }
                 }
@@ -47,13 +59,13 @@ public class Main {
         });
 
 
-        Thread[] threads = new Thread[] {threadLength3, threadLength4, threadLength5};
+        Thread[] threads = new Thread[]{threadCriteria1, threadCriteria2, threadCriteria3};
 
-        for(Thread thread: threads){
+        for (Thread thread : threads) {
             thread.start();
         }
 
-        for(Thread thread: threads){
+        for (Thread thread : threads) {
             try {
                 thread.join();
             } catch (InterruptedException e) {
@@ -75,12 +87,12 @@ public class Main {
         return text.toString();
     }
 
-    public static boolean isPalindrome(String text){
+    public static boolean isPalindrome(String text) {
         int left = 0;
         int right = text.length() - 1;
 
-        while(left < right){
-            if(text.charAt(left) != text.charAt(right)){
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
                 return false;
             }
             left++;
@@ -90,11 +102,11 @@ public class Main {
         return true;
     }
 
-    public static boolean isOneLetter(String text){
+    public static boolean isOneLetter(String text) {
         char origin = text.charAt(0);
 
-        for(char letter: text.toCharArray()){
-            if(origin != letter){
+        for (char letter : text.toCharArray()) {
+            if (origin != letter) {
                 return false;
             }
         }
@@ -102,9 +114,9 @@ public class Main {
         return true;
     }
 
-    public static boolean isAscendingRow(String text){
-        for(int i=0; i<text.length() - 1; i++){
-            if(!(text.charAt(i) <= text.charAt(i+1))){
+    public static boolean isAscendingRow(String text) {
+        for (int i = 0; i < text.length() - 1; i++) {
+            if (!(text.charAt(i) <= text.charAt(i + 1))) {
                 return false;
             }
         }
